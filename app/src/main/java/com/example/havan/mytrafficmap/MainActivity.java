71074f19ee53dd0099a2be019535d8dac388364a
THANH_PC\HaVan <thanhahvan1@gmail.com>
2017-02-12 15:00:42 +0700

    Charge colors. just to split couse1 to restructure some activities. Will be merged soon!

--- app/src/main/java/com/example/havan/mytrafficmap/MainActivity.java
+++ app/src/main/java/com/example/havan/mytrafficmap/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
+// will be write later
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
