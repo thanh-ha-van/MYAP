2d4050ab59e10678477fde6ac15d230dff6051d8
Ha  Van Thanh <thanhahvan1@gmail.com>
2017-03-20 17:48:34 +0700

    On fixing errors. Mar 20 at 6 pm.

--- app/src/main/java/com/example/havan/mytrafficmap/FavListActivity.java
+++ app/src/main/java/com/example/havan/mytrafficmap/FavListActivity.java
@@ -1,32 +1,28 @@
 package com.example.havan.mytrafficmap;
 
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.havan.mytrafficmap.Network.Object.Data;
 import com.example.havan.mytrafficmap.SQLite.DatabaseHandler;
 import com.example.havan.mytrafficmap.view.AlertDialogManager;
 import com.example.havan.mytrafficmap.view.CustomAdapter;
 import com.example.havan.mytrafficmap.SQLite.DataModel;
-import com.example.havan.mytrafficmap.view.YNDialogManager;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.model.LatLng;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -34,7 +30,6 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.WindowFeature;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -47,7 +42,8 @@ public class FavListActivity extends AppCompatActivity implements GoogleApiClien
 
     AlertDialogManager alert = new AlertDialogManager();
 
-    YNDialogManager yesNoAlert = new YNDialogManager();
+    String title = "Confirm";
+    String message= "Are you sure to delete this place?";
 
     @ViewById(R.id.show_info)
     ImageButton showInfo;
@@ -121,10 +117,14 @@ public class FavListActivity extends AppCompatActivity implements GoogleApiClien
                                 final Place myPlace = places.get(0);
                                 intent.putExtra("lat", myPlace.getLatLng().latitude);
                                 intent.putExtra("lon", myPlace.getLatLng().longitude);
-                                intent.putExtra("address", myPlace.getAddress() + "\n" + myPlace.getId());
+                                intent.putExtra("address",
+                                        myPlace.getAddress()
+                                                + "\n"
+                                                + myPlace.getId()
+                                );
 
                                 setResult(RESULT_OK, intent);
-                                onBackPressed();
+                                finish();
                             }
                             places.release();
                         }
@@ -168,7 +168,7 @@ public class FavListActivity extends AppCompatActivity implements GoogleApiClien
 
     @Click(R.id.btn_add_place)
     void addPlace() {
-        startActivity(new Intent(FavListActivity.this, SearchActivity_.class));
+        startActivity(new Intent(getApplicationContext(), SearchActivity_.class));
         finish();
     }
 
@@ -177,21 +177,60 @@ public class FavListActivity extends AppCompatActivity implements GoogleApiClien
 
         if (currentPosition != -1) {
 
-            if (yesNoAlert.showNYDialog(this, "Confirm", "Do you really want to delete this place?")) {
-                db.deletePlace(adapter.getItem(currentPosition));
+            {
+                final Dialog alertDialog = new Dialog(this);
+                alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                alertDialog.getWindow()
+                        .setBackgroundDrawable(new ColorDrawable(
+                                        android
+                                                .graphics
+                                                .Color
+                                                .TRANSPARENT
+                                )
+                        );
+                alertDialog.setCancelable(false);
+                alertDialog.setContentView(R.layout.yes_no_dialog_layout);
+
+                TextView tvTitle = (TextView) alertDialog.findViewById(R.id.tv_title);
+                tvTitle.setText(title);
+
+                TextView tvContent = (TextView) alertDialog.findViewById(R.id.tv_content);
+                tvContent.setText(message);
+
+                ImageButton dialogButton = (ImageButton)
+                        alertDialog.findViewById(R.id.btn_ok);
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        alertDialog.dismiss();
+                        db.deletePlace(adapter.getItem(currentPosition));
+                        afterViews();
+
+                    }
+                });
+
+                ImageButton cancelButton = (ImageButton)
+                        alertDialog.findViewById(R.id.btn_cancel);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.cancel();
+                    }
+                });
+                alertDialog.show();
+
             }
-        }
+        } else Toast.makeText(
+                FavListActivity.this,
+                "Please choose a place first!",
+                Toast.LENGTH_SHORT
+        ).
 
-    else Toast.makeText(
-    FavListActivity.this,
-            "Please choose a place first!",
-    Toast.LENGTH_SHORT
-    ).
-
-    show();
+                show();
 
 
-}
+    }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
